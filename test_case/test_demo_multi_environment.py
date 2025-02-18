3e3ced8273bfa62e05ada63d05d034b086a79346
jason007 <945514579@qq.com>
2025-02-18 12:38:48 +0800

    Update GitHub test_case/test_demo_multi_environment.py

--- test_case/test_demo_multi_environment.py
+++ test_case/test_demo_multi_environment.py
@@ -20,7 +20,8 @@ class TestPytestMultiEnvDemo:
         post_api_request_data = env_request_data["postAPI"]
         post_api_response_data = env_response_data["postAPI"]
         # send request
-        response = requests.post(host + post_api, post_api_request_data)
+        response = requests.post(host + post_api, json=post_api_request_data)
         # assert
         assert response.status_code == 201
         assert response.json() == post_api_response_data
+    
